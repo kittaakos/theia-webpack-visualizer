19174e8aaa2b39c6072994419c3a35a33c75c339
Dennis Huebner <dennis.huebner@gmail.com>
2017-12-13 15:24:32 +0100

    Opener service and handler enablement

--- theia-webpack-visualizer-extension/src/browser/theia-webpack-visualizer-contribution.ts
+++ theia-webpack-visualizer-extension/src/browser/theia-webpack-visualizer-contribution.ts
@@ -3,6 +3,8 @@ import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegist
 import { NAVIGATOR_CONTEXT_MENU } from "@theia/navigator/lib/browser/navigator-menu";
 import { WebpackVisualizer } from "../common/index";
 import { UriSelection } from '@theia/filesystem/lib/common/index';
+import { OpenerService } from "@theia/core/lib/browser";
+import URI from "@theia/core/lib/common/uri";
 
 export const TheiaWebpackVisualizerCommand = {
     id: 'TheiaWebpackVisualizer.command',
@@ -19,6 +21,7 @@ export class TheiaWebpackVisualizerCommandContribution implements CommandContrib
     constructor(
         @inject(MessageService) private readonly messageService: MessageService,
         @inject(SelectionService) private readonly selectionService: SelectionService,
+        @inject(OpenerService) private readonly openerService: OpenerService,
         @inject(WebpackVisualizer) private readonly webstatService: WebpackVisualizer
     ) { }
 
@@ -30,8 +33,19 @@ export class TheiaWebpackVisualizerCommandContribution implements CommandContrib
                     const data = await this.webstatService.getDependencyData(selection.uri.toString());
                     if (data) {
                         this.messageService.info(data);
+                        const toOpen = new URI('file://' + data)
+                        this.openerService.getOpener(toOpen).then(opener => {
+                           opener.open(toOpen)
+                        })
                     }
                 }
+            },
+            isEnabled:  ()=> {
+                const selection = this.selectionService.selection
+                if (UriSelection.is(selection)) {
+                    return selection.uri.toString().toLowerCase().endsWith('webpack.config.js')
+                }
+                return false
             }
         });
     }
