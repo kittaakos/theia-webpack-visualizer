576be7810ceb7a08bc64ad5a2384acd80b6fb944
Akos Kitta <kittaakos@gmail.com>
2017-12-13 09:36:58 +0100

    Added skeleton for the tests.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

---
+++ theia-webpack-visualizer-extension/src/node/default-webpack-visualizer.spec.ts
@@ -0,0 +1,14 @@
+import 'mocha';
+import { expect } from 'chai';
+
+describe('webpack-visualizer', () => {
+
+    it('generate', () => {
+        expect(true).to.be.true;
+    });
+
+    it('bumm!', () => {
+        expect(true).to.be.true;
+    });
+
+});
--- theia-webpack-visualizer-extension/src/node/default-webpack-visualizer.ts
+++ theia-webpack-visualizer-extension/src/node/default-webpack-visualizer.ts
@@ -3,11 +3,13 @@ import { injectable } from "inversify";
 import { WebpackVisualizer } from "../common";
 import URI from "@theia/core/lib/common/uri";
 
+const Visualizer: any = require('webpack-visualizer-plugin');
+
 @injectable()
 export class DefaultWebpackVisualizer implements WebpackVisualizer {
 
     async getDependencyData(confFileUri: string): Promise<string | undefined> {
-       const selectedFile = new URI(confFileUri);
-       return "{data:'"+ selectedFile +"'}";
+        const selectedFile = new URI(confFileUri);
+        return "{data:'" + selectedFile.path + Visualizer + "'}";
     }
 }
