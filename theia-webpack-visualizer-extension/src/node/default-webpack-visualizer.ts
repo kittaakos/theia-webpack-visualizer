46064ea312fe4183e13aadee08e93990a5cadef6
Akos Kitta <kittaakos@gmail.com>
2017-12-13 14:53:27 +0100

    Added typings for the visualizer plug-in.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- theia-webpack-visualizer-extension/src/node/default-webpack-visualizer.ts
+++ theia-webpack-visualizer-extension/src/node/default-webpack-visualizer.ts
@@ -1,16 +1,26 @@
-
 import { injectable } from "inversify";
-import { WebpackVisualizer } from "../common";
+import { Compiler, MultiCompiler } from 'webpack';
 import URI from "@theia/core/lib/common/uri";
+import { WebpackVisualizer } from "../common";
 
-const Visualizer: any = require('webpack-visualizer-plugin');
+export const WebpackVisualizerPlugin: WebpackVisualizerPlugin = require('webpack-visualizer-plugin');
 
 @injectable()
 export class DefaultWebpackVisualizer implements WebpackVisualizer {
 
     async getDependencyData(confFileUri: string): Promise<string | undefined> {
         const selectedFile = new URI(confFileUri);
-        log(Visualizer);
-        return "{data:'" + selectedFile.path + "'}";
+        return "{data:'" + selectedFile.path + WebpackVisualizerPlugin + "'}";
     }
+}
+
+export interface WebpackVisualizerPlugin {
+    new(options?: WebpackVisualizerPlugin.Options): WebpackVisualizerPlugin;
+    apply(compiler: Compiler | MultiCompiler): void;
+}
+
+export namespace WebpackVisualizerPlugin {
+    export interface Options {
+        filename: string
+    }    
 }
